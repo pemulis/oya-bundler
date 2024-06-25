43528a834733734a3e85f6ff9f67c3e42756640a
John Shutt <pemulis@users.noreply.github.com>
2024-06-25 14:51:04 -0700

    remove unused util import
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- handlers.js
+++ handlers.js
@@ -1,7 +1,6 @@
 // Load environment variables
 require('dotenv').config();
 
-const util = require('util');
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');

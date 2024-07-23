1fb8c1fc663070b76f4a4b3795c2f08e5878f456
John Shutt <pemulis@users.noreply.github.com>
2024-07-23 11:01:50 -0700

    simplify initial testnet token list
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- handlers.js
+++ handlers.js
@@ -237,8 +237,7 @@ async function initializeAccount(account) {
       const supportedTokens18 = [
         "0x0000000000000000000000000000000000000000", // raw ETH
         "0x04Fa0d235C4abf4BcF4787aF4CF447DE572eF828", // UMA
-        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", // WETH
-        "0x6B175474E89094C44Da98b954EedeAC495271d0F" // DAI
+        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" // WETH
       ];
       const supportedTokens6 = [
         "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" // USDC

23705208458784a71adb04551761f44166cc284a
John Shutt <pemulis@users.noreply.github.com>
2024-07-22 17:05:28 -0700

    try to handle 18 decimals for test token balance initialization
    Signed-off-by: John Shutt <pemulis@users.noreply.github.com>

--- handlers.js
+++ handlers.js
@@ -170,7 +170,8 @@ const supportedTokens = [
   "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", // USDC
   "0x6B175474E89094C44Da98b954EedeAC495271d0F" // DAI
 ];
-const initialBalance = 1000000 * i18; // Initial token balance to assign to new accounts
+// Initial token balance to assign to new accounts, to 18 decimal places
+const initialBalance = 1000000 * 10^18;
 
 async function updateBalances(from, to, token, amount) {
   try {
